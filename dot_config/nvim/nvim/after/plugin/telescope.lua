a97d0b6e0682a27e345f898ba899174a2c3d84f2
Erik Dzotsenidze <erdzot@pop-os.localdomain>
2023-08-01 13:58:09 +0300

    fix repo

--- dot_config/nvim/nvim/after/plugin/telescope.lua
+++
@@ -1,9 +0,0 @@
-require("telescope")
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "Search files"})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>pw', builtin.grep_string, { desc = "Search current word"})
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, { desc = "Search by grep"})
-
-require('telescope').load_extension('ui-select')
--- dot_config/nvim/nvim/init.lua
+++
@@ -1,6 +0,0 @@
--- vim.g.loaded_netrw = 1
--- vim.g.loaded_netrwPlugin = 1
-if vim.g.vscode then
-else
-    require("erdzot")
-end
