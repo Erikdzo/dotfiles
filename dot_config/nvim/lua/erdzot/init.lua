297a80e5d0b272677c7f04b6b28266c01348a702
Erik Dzotsenidze <erdzot@pop-os.localdomain>
2023-08-01 15:39:57 +0300

    help -> vimdoc

--- dot_config/nvim/lua/erdzot/init.lua
+++ dot_config/nvim/lua/erdzot/init.lua
@@ -2,13 +2,13 @@ require("erdzot.remap")
 require("erdzot.set")
 require("erdzot.packer")
 
-require 'nvim-treesitter.install'.compilers = { "clang","zig" }
+-- require 'nvim-treesitter.install'.compilers = { "clang","zig" }
 
 -- require("rose-pine").setup({
 --     disable_background = true
 -- })
 -- vim.cmd("colorscheme rose-pine")
 require("onedark").setup {
-    transparent = false 
+    transparent = false
 }
 require("onedark").load()
