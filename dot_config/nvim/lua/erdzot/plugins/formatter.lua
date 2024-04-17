591d0b4c7c6fba4d409dee0ca5d678fa45f1c98a
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-04-17 10:53:32 +0300

    update

--- dot_config/nvim/lua/erdzot/plugins/formatter.lua
+++ dot_config/nvim/lua/erdzot/plugins/formatter.lua
@@ -1,20 +1,29 @@
 return {
     {
         'stevearc/conform.nvim',
+        dependencies = {
+            'folke/neoconf.nvim',
+        },
         opts = {
             formatters_by_ft = {
                 lua = { "stylua" },
                 python = { "black" },
                 javascript = { { "prettierd", "prettier" } },
-                vue = { { "prettierd", "prettier" } },
                 typescript = { { "prettierd", "prettier" } },
+                vue = function()
+                    local neoconf = require("neoconf")
+                    if neoconf.get("eslint.enabled") then
+                        return {}
+                    end
+                    return { { "prettierd", "prettier" } }
+                end,
                 html = { { "prettierd", "prettier" } },
                 css = { { "prettierd", "prettier" } },
                 scss = { { "prettierd", "prettier" } },
             },
             format_on_save = {
                 timeout_ms = 500,
-                lsp_fallback = true,
+                lsp_fallback = "always",
             },
         },
     },
