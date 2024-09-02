69c6fc79941700d9d93df44d8b04069a186d4815
Erik Dzotsenidze <erikdzotsenidze@gmail.com>
2024-09-02 10:03:47 +0300

    remove copilot

--- dot_config/nvim/lua/erdzot/plugins/copilot.lua
+++ dot_config/nvim/lua/erdzot/plugins/copilot.lua
@@ -1,8 +1,8 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        opts = { suggestion = { auto_trigger = true } }
-    },
+    -- {
+    --     "zbirenbaum/copilot.lua",
+    --     cmd = "Copilot",
+    --     event = "InsertEnter",
+    --     opts = { suggestion = { auto_trigger = true } }
+    -- },
 }
